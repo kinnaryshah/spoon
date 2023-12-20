eb7ef7e41a2b6c028ff14137956759e7b98f4aae
Kinnary Shah <kinnaryshahh@gmail.com>
2023-12-20 14:34:43 -0500

    add weighted nnSVG sub function for matrix input

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,35 @@
 #sub function for weighted_nnSVG()
 
-weighted_nnSVG_calc <- function(spe, w, i){
+weighted_nnSVG_calc_spe <- function(spe, w, i){
   res = tryCatch({
     print(i)
     weight_output_i <- nnSVG(spe[i,], X=matrix(w[,i]), assay_name = "weighted_logcounts")
-    list(weighted_rank = rowData(weight_output_i)$rank,
-         weighted_LR_stat = rowData(weight_output_i)$LR_stat,
+    list(weighted_LR_stat = rowData(weight_output_i)$LR_stat,
          weighted_sigma.sq = rowData(weight_output_i)$sigma.sq,
          weighted_tau.sq = rowData(weight_output_i)$tau.sq,
          weighted_prop_sv = rowData(weight_output_i)$prop_sv)
   }, error=function(e){
     cat("ERROR :",conditionMessage(e), "\n")
-    list(weighted_rank = NA,
-         weighted_LR_stat = NA,
+    list(weighted_LR_stat = NA,
+         weighted_sigma.sq = NA,
+         weighted_tau.sq = NA,
+         weighted_prop_sv = NA)
+  })
+  return(res)
+}
+
+weighted_nnSVG_calc_mat <- function(w_logcounts_mat, coords, w, i){
+  res = tryCatch({
+    print(i)
+    weight_output_i <- nnSVG(input = t(as.matrix(w_logcounts_mat[i,])), spatial_coords = coords, X=matrix(w[,i]))
+
+    list(weighted_LR_stat = weight_output_i[11],
+         weighted_sigma.sq = weight_output_i[1],
+         weighted_tau.sq = weight_output_i[2],
+         weighted_prop_sv = weight_output_i[9])
+  }, error=function(e){
+    cat("ERROR :",conditionMessage(e), "\n")
+    list(weighted_LR_stat = NA,
          weighted_sigma.sq = NA,
          weighted_tau.sq = NA,
          weighted_prop_sv = NA)
