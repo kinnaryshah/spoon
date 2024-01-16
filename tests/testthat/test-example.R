394173f112f4958cd8ea6b6e802e47db1077fef4
Kinnary Shah <kinnaryshahh@gmail.com>
2024-01-16 11:39:10 -0500

    add unit tests

---
+++ tests/testthat/test-example.R
@@ -0,0 +1,12 @@
+# run examples from function documentation
+example(generate_weights, echo = FALSE)
+
+test_that("example object has correct class", {
+  expect_s4_class(spe_results, "SpatialExperiment")
+})
+
+test_that("example objects have correct dimensions", {
+  expect_equal(dim(spe_results), c(6, 3620))
+  expect_equal(dim(results), c(6, 6))
+})
+
